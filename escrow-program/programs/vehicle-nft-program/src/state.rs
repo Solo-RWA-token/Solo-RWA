0b06390888fd0bf580c851ec2bfee5dc43a5bf7b
anurag <anurags9693@gmail.com>
2026-03-29 18:46:33 +0530

    Refactor VehicleMetadata structure in vehicle NFT program
    - Changed the `vin_hash` field to `vin` of type `String` for better readability and flexibility.
    - Renamed `escrow_pda` to `order_pda` to clarify its purpose in the context of vehicle orders.
    - Updated the `SPACE` constant to reflect the new data types and sizes in the `VehicleMetadata` struct.

--- escrow-program/programs/vehicle-nft-program/src/state.rs
+++ escrow-program/programs/vehicle-nft-program/src/state.rs
@@ -11,16 +11,16 @@ pub enum VehicleStatus {
 
 #[account]
 pub struct VehicleMetadata {
-    pub vin_hash: [u8; 32],
+    pub vin: String,
     pub model: String,
     pub color: String,
     pub status: u8, // Using u8 to map to VehicleStatus
-    pub escrow_pda: Pubkey,
+    pub order_pda: Pubkey,
     pub buyer_wallet: Pubkey,
     pub delivery_est: String,
     pub bump: u8,
 }
 
 impl VehicleMetadata {
-    pub const SPACE: usize = 8 + 32 + (4 + 32) + (4 + 32) + 1 + 32 + 32 + (4 + 32) + 1;
+    pub const SPACE: usize = 8 + (4 + 64) + (4 + 32) + (4 + 32) + 1 + 32 + 32 + (4 + 32) + 1;
 }
